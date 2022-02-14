b051af2a5b22bdee75992b9fe4b0ade587e27302
Elena <helensita.palma1@gmail.com>
2022-02-13 22:40:55 -0500

    Changed uri Mongo

--- src/database/DBConnection.js
+++ src/database/DBConnection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-const uri = "mongodb://127.0.0.1:27017/crm-clientes"
+const uri = "mongodb+srv://elena2900:u0KbuQOUtW55ZZGJ@curso-mongodb.ncvpw.mongodb.net/crm-clientes"
 
 const DBConnection = async()=>{
     try{
